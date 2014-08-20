a591d35464505c3455d656bf7919dc33a8ed45a3
vsn4ik <vsn4ik@gmail.com>
2014-08-21 00:45:23 +0400

    Add scroll to top button.

--- docs/assets/js/index.js
+++ docs/assets/js/index.js
@@ -2,4 +2,13 @@
 
 $(function() {
 	$('.dropdown-submenu > a').submenupicker();
+
+	$('#scroll_top').click(function() {
+		// 'html' for Mozilla Firefox, 'body' for other browsers
+		$('body, html').animate({
+			scrollTop: 0
+		}, 800);
+
+		this.blur();
+	});
 });
