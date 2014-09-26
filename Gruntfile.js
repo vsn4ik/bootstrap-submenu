ff6f1434bb08233f0e99cc4d4cd7804f34042b62
vsn4ik <vsn4ik@gmail.com>
2014-09-26 19:34:52 +0400

    Grunt: Update clean task.

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,7 +7,7 @@ module.exports = function(grunt) {
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
 		clean: {
-			dist: 'dist'
+			dist: ['dist', '*-dist.zip']
 		},
 		less: {
 			core: {
