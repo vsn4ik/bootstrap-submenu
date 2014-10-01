df7528b590f527ec2144e8299a9c8b4da353fae3
vsn4ik <vsn4ik@gmail.com>
2014-10-02 00:13:43 +0400

    Bump v1.2.3.

--- dist/js/bootstrap-submenu.js
+++ dist/js/bootstrap-submenu.js
@@ -1,16 +1,21 @@
 /*!
- * Bootstrap-submenu v1.2.2 (http://vsn4ik.github.io/bootstrap-submenu)
+ * Bootstrap-submenu v1.2.3 (http://vsn4ik.github.io/bootstrap-submenu)
  * Copyright 2014 Vasily A. (https://github.com/vsn4ik)
  * Licensed under MIT (https://github.com/vsn4ik/bootstrap-submenu/blob/master/LICENSE)
  */
 
 'use strict';
 
-if (typeof jQuery === 'undefined') {
-	throw new Error('Bootstrap-submenu requires jQuery');
-}
-
-(function($) {
+(function(factory) {
+	if (typeof define == 'function' && define.amd) {
+		// AMD. Register as an anonymous module depending on jQuery
+		define(['jquery'], factory);
+	}
+	else {
+		// No AMD. Register plugin with global jQuery object
+		factory(jQuery);
+	}
+})(function($) {
 	var desc = ':not(.disabled, .divider, .dropdown-header)';
 
 	function Submenupicker(element) {
@@ -149,4 +154,4 @@ if (typeof jQuery === 'undefined') {
 			}
 		});
 	};
-})(jQuery);
+});
