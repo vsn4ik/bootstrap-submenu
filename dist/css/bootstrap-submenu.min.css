e798705d511ea32bf17ebe84ea0c8e20942933e5
vsn4ik <vsn4ik@gmail.com>
2014-11-17 11:58:50 +0300

    Bump 1.2.7.

--- dist/css/bootstrap-submenu.min.css
+++ dist/css/bootstrap-submenu.min.css
@@ -1,5 +1,5 @@
 /*!
- * Bootstrap-submenu v1.2.6 (http://vsn4ik.github.io/bootstrap-submenu)
+ * Bootstrap-submenu v1.2.7 (http://vsn4ik.github.io/bootstrap-submenu)
  * Copyright 2014 Vasily A. (https://github.com/vsn4ik)
  * Licensed under MIT (https://github.com/vsn4ik/bootstrap-submenu/blob/master/LICENSE)
  */
