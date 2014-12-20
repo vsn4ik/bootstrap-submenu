80e400e2eb83a2ae157c07d871dfb8b321de20d6
vsn4ik <vsn4ik@gmail.com>
2014-12-20 03:40:53 +0300

    Update jquery.

--- dist/js/bootstrap-submenu.js
+++ dist/js/bootstrap-submenu.js
@@ -149,7 +149,7 @@
 
   // For AMD/Node/CommonJS used elements (optional)
   // http://learn.jquery.com/jquery-ui/environments/amd/
-  $.fn.submenupicker = function(elements) {
+  return ($.fn.submenupicker = function(elements) {
     var $elements = this instanceof $ ? this : $(elements);
 
     return $elements.each(function() {
@@ -161,7 +161,5 @@
         $.data(this, 'bs.submenu', data);
       }
     });
-  };
-
-  return $.fn.submenupicker;
+  });
 });
