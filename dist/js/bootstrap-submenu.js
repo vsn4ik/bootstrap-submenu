5106ee0428c417d9ae8e013abd64c8abc3634080
vsn4ik <vsn4ik@gmail.com>
2015-03-14 18:33:43 +0300

    dist

--- dist/js/bootstrap-submenu.js
+++ dist/js/bootstrap-submenu.js
@@ -40,13 +40,16 @@
 
   Submenupicker.prototype = {
     init: function() {
-      this.$element.on('click.bs.dropdown', this.click.bind(this));
-      this.$element.keydown(this.keydown.bind(this));
+      this.$element.on({
+        'click.bs.dropdown': this.click.bind(this),
+        keydown: this.keydown.bind(this)
+      });
+
       this.$menu.on('hide.bs.submenu', this.hide.bind(this));
-      this.$items.keydown(this.item_keydown.bind(this));
+      this.$items.on('keydown', this.item_keydown.bind(this));
 
       // Bootstrap fix
-      this.$menu.nextAll(desc + ':first:not(.dropdown-submenu)').children('a').keydown(this.next_keydown.bind(this));
+      this.$menu.nextAll(desc + ':first:not(.dropdown-submenu)').children('a').on('keydown', this.next_keydown.bind(this));
     },
     click: function(event) {
       event.stopPropagation();
@@ -93,7 +96,7 @@
           }
           else {
             this.$menus.trigger('hide.bs.submenu');
-            this.$drop.removeClass('open').children('a').focus();
+            this.$drop.removeClass('open').children('a').trigger('focus');
           }
         }
         else {
@@ -111,7 +114,7 @@
             return;
           }
 
-          $items.eq(index).focus();
+          $items.eq(index).trigger('focus');
         }
       }
     },
@@ -125,7 +128,7 @@
       event.stopPropagation();
 
       this.close();
-      this.$element.focus();
+      this.$element.trigger('focus');
     },
     next_keydown: function(event) {
       // 38: Arrow up
@@ -144,13 +147,13 @@
 
       var index = $items.index(event.target);
 
-      $items.eq(index - 1).focus();
+      $items.eq(index - 1).trigger('focus');
     }
   };
 
   // For AMD/Node/CommonJS used elements (optional)
   // http://learn.jquery.com/jquery-ui/environments/amd/
-  return ($.fn.submenupicker = function(elements) {
+  return $.fn.submenupicker = function(elements) {
     var $elements = this instanceof $ ? this : $(elements);
 
     return $elements.each(function() {
@@ -162,5 +165,5 @@
         $.data(this, 'bs.submenu', data);
       }
     });
-  });
+  };
 });
