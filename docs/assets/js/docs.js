5dad18632710a9cf8c4399c93b248842790e407d
vsn4ik <vsn4ik@gmail.com>
2018-07-13 09:58:19 +0300

    Update docs

--- docs/assets/js/docs.js
+++ docs/assets/js/docs.js
@@ -25,39 +25,9 @@ document.addEventListener('DOMContentLoaded', function() {
     }
   });
 
-  /**
-   * document.documentElement: 'html', for Mozilla Firefox
-   * document.body: 'body', for other browsers
-   */
-  var containers = [
-    document.body,
-    document.documentElement
-  ];
-
-  var scrollButtonNode = document.querySelector('#scroll-top');
-
-  function updateScrollButtonVisibility() {
-    var scrollTop = containers.reduce(function(result, element) {
-      return result + element.scrollTop;
-    }, 0);
-
-    scrollButtonNode.classList.toggle('hidden', scrollTop < 100);
-  }
-
-  scrollButtonNode.addEventListener('click', function() {
-    window.onscroll = null;
-
-    scrollButtonNode.classList.add('hidden');
-
-    // 'html' for Mozilla Firefox, 'body' for other browsers
-    $(containers).animate({
-      scrollTop: 0
-    }, 500, $.proxy(function() {
-      window.onscroll = updateScrollButtonVisibility;
-    }, this));
-  });
-
-  window.onscroll = updateScrollButtonVisibility;
+  window.onscroll = function() {
+    document.querySelector('.js-scroll-top').hidden = window.pageYOffset < 100;
+  };
 
   // NOTE: Dropdown fix
   $('a[tabindex][data-toggle="dropdown"]').on('keydown', function(event) {
@@ -81,6 +51,4 @@ document.addEventListener('DOMContentLoaded', function() {
   });
 
   $('[data-submenu]').submenupicker();
-
-  updateScrollButtonVisibility();
 });
